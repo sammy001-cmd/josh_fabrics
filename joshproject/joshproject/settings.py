a9a1954ae066c9735c1212c8372730aedf63f4ab
sammy <samueltoluwani169@gmail.com>
2025-08-07 13:08:50 -0300

    josh_fabrics

--- joshproject/joshproject/settings.py
+++ joshproject/joshproject/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this is where collectstat
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 # Default primary key field type
